7352854b47930ab20beedb1e869724cb2802d22d
Ryan McCahon <mccahonr@yahoo.com>
2019-09-05 17:22:50 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,15 @@
 class Author
-  attr_accessor :name, :posts
+  attr_accessor :name
 
   @@post_count = 0
 
   def initialize(name)
     @name = name
     @posts = []
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
   end
 
   def add_post(post)
